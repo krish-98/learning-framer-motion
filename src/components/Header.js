93a058532ced8e01fdfd65dee865e36b076be978
krish-98 <muralikrishnanm98@gmail.com>
2022-07-15 21:35:01 +0530

    made a simple website with animations

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,56 @@
-import React from 'react';
+import React from "react"
+import { motion } from "framer-motion"
+import { Link } from "react-router-dom"
 
 const Header = () => {
   return (
     <header>
-      <div className="logo">
-        <svg className="pizza-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-          <path
-            fill="none"
-            d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
-          />
-          <path
-            fill="none"
-            d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
-          />
-        </svg>
-      </div>
-      <div className="title">
+      <Link to="/">
+        <div className="logo">
+          <motion.svg
+            initial={{ rotate: -180 }}
+            animate={{ rotate: 0 }}
+            transition={{ duration: 1 }}
+            className="pizza-svg"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 100 100"
+          >
+            <motion.path
+              initial={{
+                opacity: 0,
+                pathLength: 0,
+              }}
+              animate={{
+                opacity: 1,
+                pathLength: 1,
+              }}
+              transition={{ duration: 2, ease: "easeInOut" }}
+              fill="none"
+              d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
+            />
+            <motion.path
+              initial={{
+                opacity: 0,
+                pathLength: 0,
+              }}
+              animate={{ opacity: 1, pathLength: 1 }}
+              transition={{ duration: 2, ease: "easeInOut" }}
+              fill="none"
+              d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
+            />
+          </motion.svg>
+        </div>
+      </Link>
+      <motion.div
+        initial={{ y: -200 }}
+        animate={{ y: -10 }}
+        transition={{ type: "tween", duration: 1.2 }}
+        className="title"
+      >
         <h1>Pizza Joint</h1>
-      </div>
+      </motion.div>
     </header>
   )
 }
 
-export default Header;
+export default Header
--- src/components/Toppings.js
+++ src/components/Toppings.js
@@ -1,32 +1,77 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react"
+import { Link } from "react-router-dom"
+import { motion } from "framer-motion"
+
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    x: "100vw",
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: { type: "spring", delay: 0.5 },
+  },
+  exit: {
+    x: "-100vh",
+    transition: { ease: "easeInOut" },
+  },
+}
+
+const buttonVariants = {
+  hover: {
+    scale: 1.1,
+    textShadow: "0px 0px 8px rgb(255,255,255)",
+    boxShadow: "0px 0px 8px rgb(255,255,255)",
+    transition: {
+      duration: 0.3,
+      yoyo: Infinity,
+    },
+  },
+}
 
 const Toppings = ({ addTopping, pizza }) => {
-  let toppings = ['mushrooms', 'peppers', 'onions', 'olives', 'extra cheese', 'tomatoes'];
+  let toppings = [
+    "mushrooms",
+    "peppers",
+    "onions",
+    "olives",
+    "extra cheese",
+    "tomatoes",
+  ]
 
   return (
-    <div className="toppings container">
-      
+    <motion.div
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+      className="toppings container"
+    >
       <h3>Step 2: Choose Toppings</h3>
       <ul>
-        {toppings.map(topping => {
-          let spanClass = pizza.toppings.includes(topping) ? 'active' : '';
+        {toppings.map((topping) => {
+          let spanClass = pizza.toppings.includes(topping) ? "active" : ""
           return (
-            <li key={topping} onClick={() => addTopping(topping)}>
-              <span className={spanClass}>{ topping }</span>
-            </li>
+            <motion.li
+              whileHover={{ scale: 1.3, originX: 0, color: "yellow" }}
+              transition={{ type: "spring", stiffness: 300 }}
+              key={topping}
+              onClick={() => addTopping(topping)}
+            >
+              <span className={spanClass}>{topping}</span>
+            </motion.li>
           )
         })}
       </ul>
 
       <Link to="/order">
-        <button>
+        <motion.button variants={buttonVariants} whileHover="hover">
           Order
-        </button>
+        </motion.button>
       </Link>
-
-    </div>
+    </motion.div>
   )
 }
 
-export default Toppings;
+export default Toppings
